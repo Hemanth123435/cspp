ef3b3838e5803edc268c67175eb23ffd6e8c11ef
Hemanthram <hemanth@hemanthram@064mistprogram.net>
2024-06-10 15:50:23 +0530

    determine_winner completed

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,4 +10,33 @@ def getcomputerChoice():
     computerchioce = random.choice(choices)
     return computerchioce
 
-print(getcomputerChoice())
+# print(getcomputerChoice())
+
+def determinewinner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("tie")
+    if user_choice == "rock":
+        if computer_choice == "paper":
+            print("computer wins")
+    if user_choice == "paper":
+        if computer_choice == "scissors":
+            print("computer wins")
+    if user_choice == "scissors":
+        if computer_choice == "rock":
+            print("computer wins")
+    if user_choice == "paper":
+        if computer_choice == "rock":
+            print("user wins")
+    if user_choice == "scissors":
+        if computer_choice == "paper":
+            print("user wins")
+    if user_choice == "rock":
+        if computer_choice == "scissors":
+            print("user wins")
+    
+# user_choice = getUserchoice()
+# computer_choice = getcomputerChoice()
+# data=determinewinner(user_choice, computer_choice)
+# print(data)
+
+
