b7767b74c7092ce4bdfffb3da190c3d00e0bc27c
Hemanthram <hemanth@hemanthram@064mistprogram.net>
2024-06-10 16:01:15 +0530

    computer choice is completed

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,12 @@
+import random
 def getUserChoice():
     choice = input("enter choice= ")
     return choice
-print(getUserChoice())
+# print(getUserChoice())
+
+def getcomputerChoice():
+    choices =(["rock","paper","scissors"])
+    computerchioce = random.choice(choices)
+    return computerchioce
+
+print(getcomputerChoice())
